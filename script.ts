c27a13683a0ced0728ad08d10f83f13cdf9f6041
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-11-12 20:44:19 +0100

    ft: add first three tasks

--- script.ts
+++ script.ts
@@ -1,16 +1,33 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-// A `main` function so that you can use async/await
+// Task 1
+const getAllUsers = async () => {
+  return await prisma.user.findMany();
+};
+
+// Task 2
+const createNewUser = async (email: string, name?: string) => {
+  return await prisma.user.create({ data: { email, name } });
+};
+
+// Task 3
+const updateUser = async (email: string, input: Prisma.UserUpdateInput) => {
+  return await prisma.user.update({ where: { email }, data: input });
+};
+
 async function main() {
-  // ... you will write your Prisma Client queries here
+  // console.log(await getAllUsers());
+  // console.log(await createNewUser("s@mail.com"));
+  // console.log(await createNewUser("q@mail.com", "Qwerty"));
+  // console.log(await updateUser("s@mail.com", { name: "Alice" }));
 }
 
 main()
-  .catch(e => {
-    throw e
+  .catch((e) => {
+    throw e;
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
